1a707e19d1c8958cf34256fa5d46837d728671fc
oz <osazeowie0@gmail.com>
2022-06-25 10:35:51 +0100

    update validate function

--- script.js
+++ script.js
@@ -1,9 +1,43 @@
-function valid() {
-var fname = document.getElementById("fname");
 
-if (fname.value.length < 6) {
-   fname.className = "red"
+const valid = () => {
+   const fname = document.getElementById("fname");
+   const lname = document.getElementById("lname");
+   const email = document.getElementById("email");
+   const password = document.getElementById("password");
+   const textError = document.getElementsByClassName("txt");
+   
+
+if (fname.value.length == '') {
+   fname.className = "error"
+   fname.removeAttribute("placeholder")
+   textError[0].removeAttribute("hidden")
+  
+   
 }
 
+if (lname.value.length == '') {
+   lname.className = "error"
+   lname.removeAttribute("placeholder")
+   textError[1].removeAttribute("hidden")
+   
+}
+
+if (email.value.length == '') {
+   email.className = "error"
+   email.setAttribute("placeholder", "email@example/com")
+   textError[2].removeAttribute("hidden")
+   
+}
+
+if (password.value.length == '') {
+   password.className = "error"
+   password.removeAttribute("placeholder")
+   textError[3].removeAttribute("hidden")
+   return false
+}
+
+else {
+   alert("Form submitted successfully")
+}
 
 }
